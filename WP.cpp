d7f5ad96bc0921d4771a7fd38a2e5638b54c08b5
N59482 <iduginmail@ya.ru>
2022-01-28 18:57:02 +0300

    набросок алгоритма

--- WP.cpp
+++ WP.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int wateringPlants(vector<int>& plants, int capacity) 
     {
-    
+        int water = capacity; // количество воды в лейке
+        int steps = 0; // всего шагов
+
+        for(int i = 0; i < plants.size(); i++) //идём по грядке 
+            {
+                if(water => plants[i]) // если в лейке воды достаточно
+                    {
+                        steps++; 
+                    } else 
+                    {
+                        steps += (i * 2) + 1; // поход к источнику воды и обратно к следующей грядке
+                        water = capacity; // доливаем в лейку воды
+                    };
+                water = water - plants[i] // полив 
+            };
+        return steps;
     };
 
 int main()
